4ae7538bf28d03154a859b132fab78299190d91f
James Grieve <pocojames@hotmail.com>
2020-05-30 21:00:40 -0700

    Add basic tile class logic, add logic to bord to make it fill in numbers

--- board.rb
+++ board.rb
@@ -1,3 +1,34 @@
+require_relative "tile.rb"
 class Board
+    attr_accessor :grid
 #Contains the tiles, the game board
+def initialize
+    @grid = Array.new(9) {Array.new(9, "")}
 end
+
+def render
+    @grid.each_with_index do |row, i1|
+        p row
+    end
+end
+
+def from_file(textfile)
+     text_array = File.readlines(textfile).map(&:chomp).join("")
+     text_array = text_array.split("")
+     fill_grid(text_array.map(&:to_i))
+end
+
+def fill_grid(array)
+    @grid.each_with_index do |row,i1|
+        row.each_with_index do |col,i2|
+            @grid[i1][i2] = array.shift
+        end
+    end
+end
+
+end
+
+
+g = Board.new
+g.from_file("sudoku1.txt")
+g.render
--- tile.rb
+++ tile.rb
@@ -1,3 +1,7 @@
 class Tile
+    attr_accessor :value
 #Each tile of the gameboard
+def initialize(value)
+    @value = value
+end
 end
